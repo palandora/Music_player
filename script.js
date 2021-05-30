9c117e953319134dbb287e03e6f50ee5457273f9
palandora <bernd@dotsandlines.io>
2021-05-30 20:06:26 +0200

    updated js

--- script.js
+++ script.js
@@ -7,12 +7,13 @@ const progressContainer = document.querySelector(".progressind")
 const progress = document.querySelector(".progress")
 const labelCurrentTime = document.querySelector("#currentTime")
 const labelRemainingTime = document.querySelector("#remainingTime")
-
 const currentTitle = document.querySelector("#current_title")
 const currentArtist = document.querySelector("#current_artist")
 const currentCover = document.querySelector("#current_cover");
+const trackList = document.getElementsByClassName("track")
 
-let trackIndex = 0;
+
+let trackIndex = 1;
 
 const tracks = ["Guarda come dondolo","Happy ending","Nel blu dipinto di blu",
     "Piu di te","Un bacio a mezzanotte"]
@@ -21,11 +22,31 @@ const artists = ["Eduardo Vianello", "Alex Cameron", "Marino Marini",
 
 loadAudio(0)
 
+function highlightTracklist(direction){
+    if(direction == "forward"){
+        if(trackIndex != 0) trackList[trackIndex-1].classList.remove("selected")
+        trackList[trackIndex].classList.add("selected")
+    }else{
+        console.log("in")
+        if(trackIndex != tracks.length-1) {
+            console.log("in")
+            trackList[trackIndex+1].classList.remove("selected")
+        }
+        trackList[trackIndex].classList.add("selected")
+    }
+}
+
+
+
 function loadAudio(trackIndex){
     audio.src = `tracks/${tracks[trackIndex]}.mp3`
     currentCover.src = `covers/${tracks[trackIndex]}.jpg`
     currentTitle.innerText = tracks[trackIndex]
     currentArtist.innerText = artists[trackIndex]
+}
+
+function highlightCurrentTrack(){
+
 }
 
 
@@ -51,7 +72,8 @@ function updateProgress(e){
     //set label time remaining
     currentTime == 0 ? labelRemainingTime.innerText = "00:00" :
         labelRemainingTime.innerText = formatTime(remainingTime)
-
+    //nextSong
+    if(remainingTime==0) nextSong()
 }
 
 function pauseSong(){
@@ -64,6 +86,7 @@ function nextSong(){
     if(trackIndex>tracks.length-1){
         trackIndex = 0;
     }
+    highlightTracklist("forward")
     loadAudio(trackIndex)
     playSong()
 }
@@ -73,6 +96,7 @@ function prevSong(){
     if(trackIndex<0){
         trackIndex = tracks.length-1;
     }
+    highlightTracklist()
     loadAudio(trackIndex)
     playSong()
 }
@@ -81,6 +105,14 @@ function shuffle(){
     trackIndex = Math.floor(Math.random() * tracks.length)
     loadAudio(trackIndex)
     playSong()
+}
+
+function setProgress(e){
+    const widthObj = e.target.clientWidth
+    const relativePos = e.offsetX
+    const duration = audio.duration
+
+    audio.currentTime = (relativePos/widthObj)*duration
 }
 
 
@@ -99,4 +131,5 @@ audio.addEventListener("timeupdate",updateProgress)
 nextBtn.addEventListener("click",nextSong)
 previousBtn.addEventListener("click",prevSong)
 shuffleBtn.addEventListener("click",shuffle)
+progressContainer.addEventListener("mousedown", setProgress)
 
